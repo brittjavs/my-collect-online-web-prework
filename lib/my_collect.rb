b7c45efb806375ed064ed326be1fb10f68a9378c
Brittany Javalera <brittjavalera@gmail.com>
2019-05-12 01:49:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def my_collect(array)
   i = 0
-  empty_array = []
+  new_array = []
   while i < array.length
-    empty_array << yield(array[i])
+    new_array << yield(array[i])
     i += 1
   end
-  empty_array
+  new_array
 end
 
