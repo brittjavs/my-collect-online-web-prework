46e0f5056ed98edc060035ca5aaf786ff766f5a1
Brittany Javalera <brittjavalera@gmail.com>
2019-05-12 01:48:15 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,6 @@ def my_collect(array)
   empty_array = []
   while i < array.length
     empty_array << yield(array[i])
-  # you could also do it this way:
-  # name_collection << yield(array[i])
     i += 1
   end
   name_collection
